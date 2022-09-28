adb4f80c0c1512eccfe648ba0fbce0fbf641e98d
yourfriend <friend@yourfriend.lv>
2022-09-28 08:04:41 +0300

    custom urls, new frontend, link history, vpn/proxy checking redone

--- router.ts
+++ router.ts
@@ -1,6 +1,6 @@
 import { client, Context, isValidHttpUrl, proxyCheck, Router, Shortened, shortenRatelimit, Status } from "./deps.ts";
 
-export const api = new Router() // TODO: Probably split the routter into a file of its own
+export const api = new Router()
 // deno-lint-ignore no-explicit-any
 export async function renderShort(context: Context<Record<string, any>, Record<string, any>>) {
     const id = context.request.url.pathname.substring(1);
@@ -22,6 +22,8 @@ export async function renderShort(context: Context<Record<string, any>, Record<s
 
 api.post('/api/v1/shorten', async (ctx, next) => {
   const json = await ctx.request.body().value; 
+  let short = "";
+  
   if(await shortenRatelimit.isRatelimited(ctx.request.ip)) {
     ctx.response.status = Status.TooManyRequests;
     ctx.response.body = {
@@ -30,21 +32,46 @@ api.post('/api/v1/shorten', async (ctx, next) => {
     return;
   }
 
-  if(isValidHttpUrl(json?.url)) {
-    const short = crypto.randomUUID().slice(0, 8);
+  if(await proxyCheck(ctx.request.ip)) {
+    ctx.response.status = Status.BadRequest;
+    ctx.response.body = {
+      message: "VPN/Proxy."
+    };
+    await next();
+    return;
+  }
 
+  if(json?.short) {
+    if (
+      typeof json.short !== "string"
+    ) {
+      ctx.response.status = Status.BadRequest;
+      ctx.response.body = {
+        message: "Short is not a string!"
+      };
+      await next();
+    }    
+
+    if(json.short.match(/^[a-zA-Z0-9-_]{3,50}$/gm)) {
+      const maybeExists = await client.queryObject(`SELECT * FROM urls WHERE shorturl = $URL LIMIT 1`, {url: json.short});
+      if(maybeExists.rowCount == 0) { 
+        short = json.short 
+      } else {
+        ctx.response.status = Status.BadRequest;
+        ctx.response.body = {
+          message: "URL already exists!"
+        };
+        return;
+      }
+    }
+  }
+
+  if(!short) short = crypto.randomUUID().slice(0, 8);
+
+  if(isValidHttpUrl(json?.url)) {
     const maybeExists = await client.queryObject(`SELECT * FROM urls WHERE url = $URL LIMIT 1`, {url: json.url});
 
     shortenRatelimit.ratelimitFor(ctx.request.ip, 12e4);
-
-    if(await proxyCheck(ctx.request.ip)) {
-      ctx.response.status = Status.BadRequest;
-      ctx.response.body = {
-        message: "VPN/Proxy."
-      };
-      await next();
-      return;
-    }
 
     if(maybeExists.rowCount !== 0) {
       const actual = maybeExists.rows[0] as Shortened;
